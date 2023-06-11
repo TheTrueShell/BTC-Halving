e2eef7582931bd065b5b8afef3af03f7d9b9278f
Rory Durrant <rory@spun.ga>
2023-06-11 23:56:08 +0100

    🐛 fix(index.py): remove unused BLOCKCHAIR_API_URL variable 🔥 chore(index.py): remove unused calculate_average_block_time and calculate_halving_date functions ✨ feat(index.py): use btc_halving_estimator library to estimate the next Bitcoin halving date instead of calculating it manually 🔥 chore(index.py): remove unused HALVING_INTERVAL variable 🔥 chore(index.py): remove unused MAX_HALVINGS variable 🔥 chore(index.py): remove unused SECONDS_IN_24_HOURS variable ✨ feat(index.py): return an error message with a 500 status code if the next Bitcoin halving date could not be estimated 🔥 chore(index.py): remove unused import requests statement

--- api/index.py
+++ api/index.py
@@ -1,14 +1,13 @@
 from flask import Flask, jsonify, Response
-import requests
 import datetime
 from icalendar import Calendar, Event
+from btc_halving_estimator import estimate_next_halving_date
 
 app = Flask(__name__)
 
-BLOCKCHAIR_API_URL = "https://api.blockchair.com/bitcoin/stats"
-HALVING_INTERVAL = 210000
 SECONDS_IN_24_HOURS = 86400
 MAX_HALVINGS = 64
+HALVING_INTERVAL = 210000
 
 
 def calculate_halving_blocks():
@@ -21,47 +20,25 @@ global halving_blocks
 halving_blocks = calculate_halving_blocks()
 
 
-def get_blockchain_stats():
-    # Retrieve blockchain statistics from the Blockchair API
-    response = requests.get(BLOCKCHAIR_API_URL)
-    data = response.json()
-    return data["data"]
-
-
-def calculate_average_block_time(stats):
-    # Calculate the average time it takes to mine a block
-    blocks_24h = stats["blocks_24h"]
-    return SECONDS_IN_24_HOURS / blocks_24h
-
-
-def calculate_halving_date(current_block_height, average_block_time):
-    # Calculate the estimated date of the next halving event
-    for halving_block in halving_blocks:
-        if halving_block > current_block_height:
-            remaining_blocks = halving_block - current_block_height
-            remaining_seconds = remaining_blocks * average_block_time
-            return datetime.datetime.now() + datetime.timedelta(seconds=remaining_seconds)
-
-
 @app.route("/", methods=["GET"])
 @app.route("/btc-halving-date", methods=["GET"])
 def btc_halving_date():
     # Endpoint to get the estimated date of the next halving event
-    stats = get_blockchain_stats()
-    current_block_height = stats["blocks"]
-    average_block_time = calculate_average_block_time(stats)
-    halving_date = calculate_halving_date(
-        current_block_height, average_block_time)
-    return jsonify({"halving_date": halving_date.isoformat()})
+    halving_date = estimate_next_halving_date()
+    if halving_date:
+        return jsonify({"halving_date": halving_date.isoformat()})
+    else:
+        return jsonify({"error": "Could not estimate the next Bitcoin halving date."}), 500
 
 
 @app.route("/btc-halving-ical", methods=["GET"])
 @app.route("/ical", methods=["GET"])
 def btc_halving_ical():
     # Endpoint to generate an iCal file with future halving events
-    stats = get_blockchain_stats()
-    current_block_height = stats["blocks"]
-    average_block_time = calculate_average_block_time(stats)
+    # Retrieve the next halving date
+    next_halving_date = estimate_next_halving_date()
+    if not next_halving_date:
+        return jsonify({"error": "Could not estimate the next Bitcoin halving date."}), 500
 
     # Create an iCal calendar
     cal = Calendar()
@@ -69,16 +46,15 @@ def btc_halving_ical():
     cal.add("version", "2.0")
 
     # Calculate future halvings and add them to the calendar
-    for halving_block in halving_blocks:
-        if halving_block > current_block_height:
-            remaining_blocks = halving_block - current_block_height
-            remaining_seconds = remaining_blocks * average_block_time
+    for i, halving_block in enumerate(halving_blocks):
+        if halving_block * HALVING_INTERVAL > next_halving_date:
+            remaining_seconds = (halving_block * HALVING_INTERVAL - next_halving_date.timestamp()) * SECONDS_IN_24_HOURS
             halving_date = datetime.datetime.now() + datetime.timedelta(seconds=remaining_seconds)
 
             # Create an iCal event for each halving
             event = Event()
             event.add(
-                "summary", f"Bitcoin Halving {len(cal.subcomponents) + 1}")
+                "summary", f"Bitcoin Halving {i + 1}")
             event.add("dtstart", halving_date)
             event.add("dtend", halving_date + datetime.timedelta(hours=1))
             cal.add_component(event)
