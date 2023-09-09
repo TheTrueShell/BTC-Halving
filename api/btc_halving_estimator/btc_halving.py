9c0b28b025945aa68f87b5bff91d53270f0752e6
Rory Durrant <rory@spun.ga>
2023-09-09 21:12:28 +0100

    🐛 fix(btc_halving.py): fix f-string formatting in get_latest_block_data function to improve readability ✨ feat(btc_halving.py): add fallback value for average block time calculation to handle cases where data is missing or insufficient 🔨 refactor(btc_halving.py): extract weighted block time calculation into a separate function for better code organization and reusability 🔨 refactor(btc_halving.py): remove unnecessary comments and improve code formatting for better readability ✨ feat(btc_halving.py): add fallback value for next halving date estimation to handle cases where data is missing or insufficient 🔧 chore(btc_halving.py): remove unnecessary comments and improve code formatting for better readability

--- api/btc_halving_estimator/btc_halving.py
+++ api/btc_halving_estimator/btc_halving.py
@@ -14,14 +14,13 @@ BLOCKCHAIR_API_URL = "https://api.blockchair.com/bitcoin/blocks"
 def get_latest_block_data():
     logging.info("Fetching the latest block data")
     try:
-        response = requests.get(BLOCKCHAIR_API_URL + "?limit=1")
+        response = requests.get(f"{BLOCKCHAIR_API_URL}?limit=1")
         response.raise_for_status()
     except requests.exceptions.RequestException as e:
         logging.error(f"Error fetching the latest block data: {e}")
         return None
 
-    data = response.json().get('data')
-    if data:
+    if data := response.json().get('data'):
         logging.info(f"Latest block data: {data[0]}")
         return data[0]
     logging.warning("No data found for the latest block")
@@ -30,16 +29,12 @@ def get_latest_block_data():
 
 def get_average_block_time(current_block_height):
     logging.info("Calculating the average block time")
-    
+
     # Dynamically set the number of blocks for averaging based on how close the halving is
     blocks_to_halving = BTC_HALVING_INTERVAL - (current_block_height % BTC_HALVING_INTERVAL)
-    if blocks_to_halving > 10000:
-        num_blocks = 100
-    else:
-        num_blocks = 50
-    
+    num_blocks = 100 if blocks_to_halving > 10000 else 50
     request_url = f"{BLOCKCHAIR_API_URL}?s=time(desc)&limit={num_blocks}"
-    
+
     logging.info(f"Requesting data from: {request_url}")
 
     try:
@@ -51,23 +46,26 @@ def get_average_block_time(current_block_height):
 
     data = response.json().get('data')
     if data and len(data) > 1:
-        total_weight = 0
-        total_time = 0
-
-        # Weighted average, more recent blocks are given higher weight
-        for i, block in enumerate(data[:-1]):
-            block_time = parse(block['time'])
-            next_block_time = parse(data[i + 1]['time'])
-            weight = i + 1
-            total_weight += weight
-            total_time += weight * (block_time - next_block_time).total_seconds()
-
-        avg_block_time = total_time / total_weight
-        logging.info(f"Weighted average block time: {avg_block_time} seconds")
-        return avg_block_time
-
+        return get_weighted_block_time(data)
     logging.warning("Could not calculate average block time")
     return 600  # fallback to 10 minutes
+
+
+def get_weighted_block_time(data):
+    total_weight = 0
+    total_time = 0
+
+    # Weighted average, more recent blocks are given higher weight
+    for i, block in enumerate(data[:-1]):
+        block_time = parse(block['time'])
+        next_block_time = parse(data[i + 1]['time'])
+        weight = i + 1
+        total_weight += weight
+        total_time += weight * (block_time - next_block_time).total_seconds()
+
+    avg_block_time = total_time / total_weight
+    logging.info(f"Weighted average block time: {avg_block_time} seconds")
+    return avg_block_time
 
 
 def calculate_next_halving_date(current_block_height, current_block_time, avg_block_time):
@@ -86,8 +84,7 @@ def calculate_next_halving_date(current_block_height, current_block_time, avg_bl
 
 
 def estimate_next_halving_date():
-    latest_block_data = get_latest_block_data()
-    if latest_block_data:
+    if latest_block_data := get_latest_block_data():
         average_block_time = get_average_block_time(latest_block_data['id'])
         next_halving_date = calculate_next_halving_date(
             latest_block_data['id'], latest_block_data['time'], average_block_time
@@ -97,9 +94,7 @@ def estimate_next_halving_date():
 
 
 if __name__ == "__main__":
-    # Run the function and print the estimated date of the next Bitcoin halving
-    next_halving_date = estimate_next_halving_date()
-    if next_halving_date:
+    if next_halving_date := estimate_next_halving_date():
         print(f"The estimated date of the next Bitcoin halving is: {next_halving_date}")
     else:
         print("Could not estimate the date of the next Bitcoin halving")
